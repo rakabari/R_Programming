2677bba878501edc3cb584883d12185f36241ef1
rakabari <35432779+rakabari@users.noreply.github.com>
2018-03-26 07:00:08 -0400

    Add files via upload

---
+++ DESCRIPTION.R
@@ -0,0 +1,9 @@
+Package: DNAtranslation
+Type: Package
+Title: Translating DNA To Protein using R
+Version: 1.0.0
+Authors@R: "Ratilal Akabari <rakabari@mail.usf.edu> [aut, cre]"
+Description: The code given uses DNA sequence and using the universal genetic code converts it to the corresponding protein sequence.
+Depends: R (>= 3.1.2)
+License: CCO
+LazyData: true
